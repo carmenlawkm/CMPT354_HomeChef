2db7f4ffe37591719f7d12e3678faad47558c917
Carmen Law <carmenlaw08@gmail.com>
2021-03-19 18:02:51 -0700

    Linking to database

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "homechef"
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home_load():
@@ -11,8 +20,19 @@ def home_load():
 def login_load():
     return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods = ['GET','POST'])
 def register_load():
+    if request.method == 'POST':
+        userName = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO profile (email, UserName, password) VALUES (%s, %s, %s)", (email, userName, password))
+        mysql.connection.commit()
+        cur.close()
+        return "success"
+
     return render_template("register.html")
 
 
