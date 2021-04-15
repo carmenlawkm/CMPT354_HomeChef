b34af56a49893555ecf084928cfada79064822fe
Ali <ali_tohidi@sfu.ca>
2021-04-15 12:06:01 -0700

    style got updated

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ foodList = []
 def home_load():
     cur1 = mysql.connection.cursor()
     cur2 = mysql.connection.cursor()
-    cur1.execute("SELECT * FROM food, publicprofileinfo, foodreviewaverage WHERE food.PUserID = publicprofileinfo.UserID AND foodreviewaverage.foodID = food.foodID")
+    cur1.execute("SELECT * FROM food, publicprofileinfo, foodreviewaverage WHERE food.PUserID = publicprofileinfo.UserID "
+                 "AND foodreviewaverage.foodID = food.foodID")
     cur2.execute("SELECT * FROM foodingredients")
     fetch = cur1.fetchall()
     fetch2 = cur2.fetchall()
