90d4614a99101b44fe784d7a1f211c6ee5abdb1c
jzl5 <jzl5@sfu.ca>
2021-04-18 00:56:32 -0700

    Merged Reviews

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = "NoKey"
 
 app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "rush"
+app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "homechef"
 
